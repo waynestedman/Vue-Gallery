731062729bbc38161898594a5022d3dc217a99ca
Wayne Stedman <wayne@pacificwebfoundry.com>
2019-02-02 17:25:34 -0800

    1st draft

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'Vue.JS Image Gallery',
-        message: 'images will go here'
+        title: 'NEW Product #1',
+        image: './images/slm.jpg',
+        longDescript: 'the newest addition to our catalog!',
+        stockStatus: 'In Stock',
+        details: [
+          "longer lashes",
+          "easy clean up",
+          "long lasting"
+        ],
+        sale: true
     }
 });
